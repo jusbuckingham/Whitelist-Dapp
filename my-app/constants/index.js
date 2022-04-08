27d0e9e36fb3112c80ce146b02357f0d707805aa
jusbuckingham <jus.buckingham@gmail.com>
2022-04-08 09:35:04 -0700

    completed

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -1,66 +1,67 @@
 export const abi = [
     {
-      "inputs": [
+      inputs: [
         {
-          "internalType": "uint8",
-          "name": "_maxWhitelistedAddresses",
-          "type": "uint8"
-        }
+          internalType: "uint8",
+          name: "_maxWhitelistedAddresses",
+          type: "uint8",
+        },
       ],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
+      stateMutability: "nonpayable",
+      type: "constructor",
     },
     {
-      "inputs": [],
-      "name": "addAddressToWhitelist",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
+      inputs: [],
+      name: "addAddressToWhitelist",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
     },
     {
-      "inputs": [],
-      "name": "maxWhitelistedAddresses",
-      "outputs": [
+      inputs: [],
+      name: "maxWhitelistedAddresses",
+      outputs: [
         {
-          "internalType": "uint8",
-          "name": "",
-          "type": "uint8"
-        }
+          internalType: "uint8",
+          name: "",
+          type: "uint8",
+        },
       ],
-      "stateMutability": "view",
-      "type": "function"
+      stateMutability: "view",
+      type: "function",
     },
     {
-      "inputs": [],
-      "name": "numAddressesWhitelisted",
-      "outputs": [
+      inputs: [],
+      name: "numAddressesWhitelisted",
+      outputs: [
         {
-          "internalType": "uint8",
-          "name": "",
-          "type": "uint8"
-        }
+          internalType: "uint8",
+          name: "",
+          type: "uint8",
+        },
       ],
-      "stateMutability": "view",
-      "type": "function"
+      stateMutability: "view",
+      type: "function",
     },
     {
-      "inputs": [
+      inputs: [
         {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
       ],
-      "name": "whitelistedAddresses",
-      "outputs": [
+      name: "whitelistedAddresses",
+      outputs: [
         {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
+          internalType: "bool",
+          name: "",
+          type: "bool",
+        },
       ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ],;
-export const WHITELIST_CONTRACT_ADDRESS = "0x9821Ec8B576b3174c0dAa87B3cFa2e79bD4fBB47";
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
+  export const WHITELIST_CONTRACT_ADDRESS =
+    "0x9821Ec8B576b3174c0dAa87B3cFa2e79bD4fBB47";
